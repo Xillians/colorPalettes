d4edc8e14160d13ec84dc3474f6c42e2ce693cc3
Xillians <chkxillians@gmail.com>
2021-12-17 13:33:07 +0100

    file added

---
+++ src/components/ColorMain.vue
@@ -0,0 +1,34 @@
+<template>
+  asd
+  <h2>{{ hexValue }}</h2>
+  <button id="find" @click="getColorPalette">get color palette</button><br />
+  {{ colorData }}
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import { colorPalette } from "../controller/API/colorPalettes";
+
+@Options({
+  components: {
+  },
+  data() {
+    return {
+      hexValue: "FFFFFF",
+      colorSchemes: new colorPalette(),
+      colorData: "",
+    };
+  },
+  methods: {
+    async getColorPalette() {
+      const response = await this.colorSchemes.GET_schemes(this.hexValue);
+      this.colorData = JSON.stringify(response.data.colors, null, 2);
+    },
+  },
+})
+export default class ColorMain extends Vue {}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
